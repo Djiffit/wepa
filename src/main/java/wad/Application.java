a42f45b3154de64e43c9044047764324a07a16d9
Djiffit <djiffit@gmail.com>
2016-09-29 21:30:19 +0300

    Making sure broken build doesn't get deployed

--- src/main/java/wad/Application.java
+++ src/main/java/wad/Application.java
@@ -1,15 +1,6 @@
 package wad;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.AbstractEnvironment;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAsync
-@EnableScheduling
-@SpringBootApplication
-public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
